c65ddf68836095a04b9b17d2700003668b8c92aa
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-05-07 13:43:30 -0500

    Programación Formulario

--- script.js
+++ script.js
@@ -8,16 +8,49 @@
     $btnMenu.lastElementChild.classList.toggle("none");
     $menu.classList.toggle("is-active");
   });
-  
+
   // TODO: tecnica de delegaci´no de eventos
-  d.addEventListener("click",(e)=>{
-    if(!e.target.matches(".menu a")) return false;
-    
-     $btnMenu.firstElementChild.classList.remove("none");
-     $btnMenu.lastElementChild.classList.add("none");
-     $menu.classList.remove("is-active");
-    
-  })
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
 
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
 
+// TODO: ContactForm
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/64c2d378a991c4d3a23092e58230fc9b", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
 })(document);
